a85427d97101d0f5ea72198e756bcefbc6cee73c
Felix Weilbach <feweilbach@gmail.com>
2017-10-17 16:33:56 +0200

    removed

--- build_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir build_debug
-cd build_debug/
-cmake -DCMAKE_BUILD_TYPE=Debug ../
-make VERBOSE=1
--- build_release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir build_release
-cd build_release/
-cmake -DCMAKE_BUILD_TYPE=Release ../
-make

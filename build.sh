d176555ad6a8e54a7aff5fedd0d07579327e195b
Felix Weilbach <feweilbach@gmail.com>
2017-10-17 21:09:22 +0200

    removed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir build_debug
-cd build_debug/
+cd build/
 cmake -DCMAKE_BUILD_TYPE=Debug ../
 make && make install
--- clean_build.sh
+++ clean_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -Rf build_debug/
+rm -Rf build/
